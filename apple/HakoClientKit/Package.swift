418e37e84ba8352912dc2171eb6f825a7da549a7
The Hako Authors <TokenPLS@users.noreply.github.com>
2026-09-24 16:37:07 +0800

    perf: optimize Release code size and bundled icon scales

--- apple/HakoClientKit/Package.swift
+++ apple/HakoClientKit/Package.swift
@@ -1,3 +1,3 @@
 // swift-tools-version: 6.0
 import PackageDescription
-let package = Package(name: "HakoClientKit", platforms: [.iOS(.v15), .macOS(.v13), .tvOS(.v17)], products: [.library(name: "HakoClientKit", targets: ["HakoClientKit"])], dependencies: [], targets: [.target(name: "HakoClientKit", dependencies: [], resources: [.copy("Resources/RuleCatalog")])])
+let package = Package(name: "HakoClientKit", platforms: [.iOS(.v15), .macOS(.v13), .tvOS(.v17)], products: [.library(name: "HakoClientKit", targets: ["HakoClientKit"])], dependencies: [], targets: [.target(name: "HakoClientKit", dependencies: [], resources: [.copy("Resources/RuleCatalog")], swiftSettings: [.unsafeFlags(["-Osize"], .when(platforms: [.iOS], configuration: .release))])])
